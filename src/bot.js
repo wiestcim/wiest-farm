a91c41eb1a1d836d0518433dd3470e5970492b56
wiestcim <moskovalicanim@hotmail.com>
2026-07-31 00:33:19 +0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -13,7 +13,6 @@ const packageJson = require("../package.json");
 
 const fs = require("node:fs");
 const chalk = require("chalk");
-const express = require('express');
 
 const globalutil = require("./utils/globalutil.js");
 const configValidator = require("./services/configValidator.js");
