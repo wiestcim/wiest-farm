cddf103b140538d0d1e1137fdf8863e52a6d553a
wiestcim <moskovalicanim@hotmail.com>
2026-07-31 00:34:14 +0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -156,36 +156,6 @@ function rpc(type) {
         );
     }
 }
-module.exports = async (req, res) => {
-  const html = `<!DOCTYPE html>
-<html lang="tr">
-<head>
-  <meta charset="UTF-8" />
-  <title>Bot Aktif</title>
-  <style>
-    body {
-      background: #111;
-      color: #eee;
-      font-family: system-ui, sans-serif;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      height: 100vh;
-      margin: 0;
-      flex-direction: column;
-    }
-    .status { color: #4ade80; font-weight: bold; }
-  </style>
-</head>
-<body>
-  <h1>🤖 Bot Çalışıyor</h1>
-  <p class="status">Durum: Aktif</p>
-</body>
-</html>`;
-
-  res.setHeader("Content-Type", "text/html; charset=utf-8");
-  res.status(200).send(html);
-};
 
 Object.assign(client, {
     chalk,
